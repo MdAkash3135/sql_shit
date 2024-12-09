3b4c84d5539b2f03a1d953a84c0403eececb53dd
MdAkash3135 <mdeaqubmia@gmail.com>
2024-12-09 20:24:18 +0600

    create emplyee table and insert some data into it

---
+++ learn.sql
@@ -0,0 +1,13 @@
+create table employees (
+	employee_id integer,
+	emp_name varchar (20),
+	salary int
+);
+insert into employees
+values 
+(1, 'akash', 40000),
+(2, 'robin', 45000),
+(3, 'james', 50000);
+
+
+select * from employees
